82a1d996b09a4e87d20d06bedcc7dcd02cef3bec
Dauren <d.k.nurgaziev@gmail.com>
2024-11-10 11:20:25 +0500

    Created test array checker

---
+++ OnlyArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OnlyArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Java", "C++", "Python", "C#", "JavaScript", "Go" };
+            string[] newArray = {};
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    if (i < array.Length - 1)
+                    {
+                        Console.Write($"{array[i]}, ");
+                    }
+                    else
+                    {
+                        Console.Write(array[i]);
+                    }
+                }
+                
+            }
+        }
+    }
+}
